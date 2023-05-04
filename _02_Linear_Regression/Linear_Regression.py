1b6459b8a4fd343de4aaf7a19412c26961de7535
LAPTOP-FMADBJT5\Cynic <204332208@qq.com>
2023-05-04 20:57:54 +0800

    第八次

--- _02_Linear_Regression/Linear_Regression.py
+++ _02_Linear_Regression/Linear_Regression.py
@@ -10,14 +10,14 @@ except ImportError as e:
     import numpy as np
 
 def ridge(data):
-    x,y=read_data('./data/exp02/')
+    x,y=read_data()
     XTx=x.T.dot(x)
     I_p=np.identity(x.shape[1])
     I_p[0,0]=0
     XTx+=0.5*I_p
     XTy=x.T.dot(y)
     w=np.linalg.solve(XTx,XTy)
-    return w
+    return np.sum(w*data)
 def lasso(data):
   #lasso线性回归
   lasso_reg=Lasso(alpha=0.1)
